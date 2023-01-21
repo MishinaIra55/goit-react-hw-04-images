602dbe8494ee5f5a4000dded3fd4942e1f3dbdea
MishynaIra <gortenzia1987@gmail.com>
2023-01-21 17:12:27 +0200

    app переписала на хуки

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,61 +1,106 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+//
+// import { Searchbar } from './Searchbar/Searchbar';
+//
+// import { ToastContainer } from 'react-toastify';
+//
+// import { GalleryForm } from './GalleryForm';
+//
+// import { Modal } from './Modal/Modal';
+//
+//
+//
+// export class App extends Component {
+//
+//   state = {
+//     search: '',
+//     showModal: false,
+//     largeImage: ''
+//
+//   };
+//
+//   handleSearchbar = search => {
+//     this.setState( { search });
+//   };
+//
+//   //переключение модалки
+//   toggleModal = () => {
+//     this.setState((state) => (
+//         {
+//           showModal: !state.showModal
+//         }
+//       )
+//     )
+// };
+//
+//   getLargeImage = (imageURL) => {
+//   this.setState((state) => (
+//     {
+//       largeImage: imageURL
+//     }
+//     )
+//   )
+// }
+//   render() {
+//     const {search, showModal, largeImage } = this.state;
+//     return (
+//       <>
+//         <Searchbar onSubmit ={this.handleSearchbar}/>
+//         <GalleryForm searchData={search} openModal={this.toggleModal} getUrl={this.getLargeImage}/>
+//         <ToastContainer autoClose={2000}/>
+//
+//         {showModal && (
+//         <Modal onClose={this.toggleModal}>
+//           <img src={largeImage} alt="" className="Modal-image" />
+//         </Modal>
+//         )}
+//       </>
+//     )
+//   }
+//   }
+//
 
+
+import { useState } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
-
-import { ToastContainer } from 'react-toastify';
-
 import { GalleryForm } from './GalleryForm';
-
+import { ToastContainer } from 'react-toastify';
 import { Modal } from './Modal/Modal';
 
+export const App =() => {
+  const [search,setSearch] = useState('');
+  const [showModal,setShowModal] = useState(false);
+  const [largeImage,setLargeImage] = useState('');
 
 
-export class App extends Component {
-
-  state = {
-    search: '',
-    showModal: false,
-    largeImage: ''
-
+  const handleSearchbar = search => {
+    setSearch( search );
   };
 
-  handleSearchbar = search => {
-    this.setState( { search });
+
+    //переключение модалки
+  const toggleModal = () => {
+    setShowModal(!showModal)
   };
 
-  //переключение модалки
-  toggleModal = () => {
-    this.setState((state) => (
-        {
-          showModal: !state.showModal
-        }
-      )
-    )
-};
 
-  getLargeImage = (imageURL) => {
-  this.setState((state) => (
-    {
-      largeImage: imageURL
-    }
-    )
-  )
-}
-  render() {
-    const {search, showModal, largeImage } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit ={this.handleSearchbar}/>
-        <GalleryForm searchData={search} openModal={this.toggleModal} getUrl={this.getLargeImage}/>
+
+  const getLargeImage = (imageURL) => {
+    setLargeImage(imageURL)
+  }
+
+  return (
+    <>
+        <Searchbar onSubmit ={handleSearchbar}/>
+
+        <GalleryForm searchData={search} openModal={toggleModal} getUrl={getLargeImage}/>
         <ToastContainer autoClose={2000}/>
 
         {showModal && (
-        <Modal onClose={this.toggleModal}>
+        <Modal onClose={toggleModal}>
           <img src={largeImage} alt="" className="Modal-image" />
         </Modal>
         )}
-      </>
-    )
-  }
-  }
-
+    </>
+  )
+}
