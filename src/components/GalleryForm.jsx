307e788af4f1036760e2a23712d90a1dc767f340
MishynaIra <gortenzia1987@gmail.com>
2023-01-22 20:42:44 +0200

    useffect не так работате

--- src/components/GalleryForm.jsx
+++ src/components/GalleryForm.jsx
@@ -1,103 +1,182 @@
-import { Component } from 'react';
+// import { Component } from 'react';
+//
+//
+// import ErrorData from './ErrorData/ErrorData';
+// import { ImageGallery } from './ImageGallery/ImageGallery';
+// import { Loader } from './Loader/Loader';
+// import {fetchAxiosGallery} from '../services/pixibay-api';
+//
+// import styles from './GalleryForm.module.css';
+// import PropTypes from 'prop-types';
+//
+//
+// export class GalleryForm extends Component {
+//
+//   state = {
+//     images: [],
+//     error: '',
+//     status: 'idle',
+//     page: 1
+//   };
+//
+//   static propTypes = {
+//     searchData: PropTypes.string.isRequired,
+//     openModal: PropTypes.func.isRequired,
+//     getUrl: PropTypes.func.isRequired,
+//   }
+//
+//   async componentDidUpdate(prevProps, prevState, snapshot) {
+//     if (prevProps.searchData !== this.props.searchData || prevState.page !== this.state.page) {
+//       this.setState({ status: 'pending' });
+//
+//       if (prevProps.searchData !== this.props.searchData && this.state.images.length > 0) {
+//         this.setState({
+//           images: [],
+//           page: 1
+//         })
+//         return;
+//       }
+//
+//       try {
+//         const response = await fetchAxiosGallery(this.props.searchData,this.state.page);
+//         this.setState(({images}) => {
+//           const newData = response.hits.map(({ id, webformatURL, largeImageURL }) => {
+//             return {
+//               id, webformatURL, largeImageURL
+//             }
+//
+//           })
+//
+//           return {
+//             images: [...images, ...newData],
+//             status: 'idle'
+//           }
+//         });
+//
+//         if (response.hits.length === 0) {
+//           throw  new SyntaxError('Try again, the search is not correct');
+//         }
+//
+//       } catch (error) {
+//         this.setState({ error: `нет картинки ${this.props.searchData}`, status: 'rejected' });
+//       }
+//     }
+//   }
+//
+//   onLoadMore =() => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   }
+//
+//   render() {
+//     const { images, error, status, page } = this.state;
+//
+//     if (status === 'idle' && images.length === 0) {
+//       return <div className={styles.text}>Введите ваш запрос поиска</div>;
+//     }
+//
+//     if (status === 'pending' && page === 1) {
+//       return <Loader/>
+//     }
+//
+//     if (status === 'rejected') {
+//       return <ErrorData message={ error} /> ;
+//     }
+//
+//     return (<>
+//       <ImageGallery
+//         images={images}
+//         modalclick={this.props.openModal}
+//         getUrl={this.props.getUrl}
+//         status={status}
+//         load={this.onLoadMore}
+//       />
+//     </>)
+//
+//   }
+// }
 
 
-import ErrorData from './ErrorData/ErrorData';
+import { useEffect, useState } from 'react';
 import { ImageGallery } from './ImageGallery/ImageGallery';
-import { Loader } from './Loader/Loader';
-import {fetchAxiosGallery} from '../services/pixibay-api';
+import { fetchAxiosGallery } from '../services/pixibay-api';
 
 import styles from './GalleryForm.module.css';
-import PropTypes from 'prop-types';
+import { Loader } from './Loader/Loader';
+import ErrorData from './ErrorData/ErrorData';
+
+export const GalleryForm = ({ openModal, getUrl, searchData }) => {
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState('');
+  const [status, setStatus] = useState('idle');
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    console.log('first effect');
+    setImages([]);
+    setPage(1);
+  },[searchData])
+
+  useEffect(() => {
+    console.log('second effect');
+    if (searchData.length > 0) {
+      setStatus('pending');
+      getImages();
+    }
+  }, [page, searchData]);
 
 
-export class GalleryForm extends Component {
 
-  state = {
-    images: [],
-    error: '',
-    status: 'idle',
-    page: 1
-  };
-
-  static propTypes = {
-    searchData: PropTypes.string.isRequired,
-    openModal: PropTypes.func.isRequired,
-    getUrl: PropTypes.func.isRequired,
-  }
-
-  async componentDidUpdate(prevProps, prevState, snapshot) {
-    if (prevProps.searchData !== this.props.searchData || prevState.page !== this.state.page) {
-      this.setState({ status: 'pending' });
-
-      if (prevProps.searchData !== this.props.searchData && this.state.images.length > 0) {
-        this.setState({
-          images: [],
-          page: 1
-        })
-        return;
-      }
-
-      try {
-        const response = await fetchAxiosGallery(this.props.searchData,this.state.page);
-        this.setState(({images}) => {
-          const newData = response.hits.map(({ id, webformatURL, largeImageURL }) => {
+  const getImages = async () => {
+    try {
+      const response = await fetchAxiosGallery(searchData, page);
+      // console.log(response);
+      const newData = response.hits.map(({ id, webformatURL, largeImageURL }) => {
             return {
               id, webformatURL, largeImageURL
             }
+      })
 
-          })
+      setImages([...images, ...newData]);
+      setStatus('idle');
 
-          return {
-            images: [...images, ...newData],
-            status: 'idle'
-          }
-        });
 
-        if (response.hits.length === 0) {
+      if (response.hits.length === 0) {
           throw  new SyntaxError('Try again, the search is not correct');
         }
 
-      } catch (error) {
-        this.setState({ error: `нет картинки ${this.props.searchData}`, status: 'rejected' });
+    } catch (error) {
+      setError(  `нет картинки ${searchData}`);
+      setStatus('rejected');
+    }
+  };
+
+  const onLoadMore = () => {
+    setPage(page + 1);
+  };
+
+//     if (status === 'rejected') {
+//       return <ErrorData message={ error} /> ;
+//     }
+
+  return (
+    <>
+      {(status === 'idle' && images.length === 0) && <div className={styles.text}>Введите ваш запрос поиска</div> }
+      {(status === 'pending' && page === 1) && <Loader/>}
+      {status === 'rejected' && <ErrorData message={ error} />}
+      {images.length > 0 &&
+        <ImageGallery
+          images={images}
+          modalclick={openModal}
+          getUrl={getUrl}
+          status={status}
+          load={onLoadMore}
+        />
       }
-    }
-  }
 
-  onLoadMore =() => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  }
-
-  render() {
-    const { images, error, status, page } = this.state;
-
-    if (status === 'idle' && images.length === 0) {
-      return <div className={styles.text}>Введите ваш запрос поиска</div>;
-    }
-
-    if (status === 'pending' && page === 1) {
-      return <Loader/>
-    }
-
-    if (status === 'rejected') {
-      return <ErrorData message={ error} /> ;
-    }
-
-    return (<>
-      <ImageGallery
-        images={images}
-        modalclick={this.props.openModal}
-        getUrl={this.props.getUrl}
-        status={status}
-        load={this.onLoadMore}
-      />
-    </>)
-
-  }
-}
-
-
-
+    </>
+  );
+};
 
 
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,49 +1,88 @@
+// import styles from './Modal.module.css';
+// import { Component } from 'react';
+//
+// import {createPortal} from 'react-dom';
+// import PropTypes from 'prop-types';
+//
+//
+// const modalRoot = document.querySelector('#modal-root');
+//
+// export class Modal extends Component {
+//
+//   static propTypes = {
+//     onClose: PropTypes.func.isRequired,
+//     children: PropTypes.any.isRequired,
+//   }
+//
+//
+//   componentDidMount() {
+//     window.addEventListener('keydown',this.handleKeyClose );
+//   }
+//
+//   componentWillUnmount() {
+//     window.removeEventListener('keydown',this.handleKeyClose );
+//   }
+//
+//   handleKeyClose  = event => {
+//     if (event.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+//
+//   handleCloseBackdrop  = event => {
+//      if (event.currentTarget === event.target) {
+//        this.props.onClose();
+//      }
+//    };
+//
+//   render() {
+//     const { children } =this.props;
+//     return createPortal(
+//       <div className={styles.overlay} onClick={this.handleCloseBackdrop}>
+//         <div className={styles.modal}>{children}</div>
+//       </div>,
+//       modalRoot
+//     );
+//
+//   }
+// }
+//
+
+import { createPortal } from 'react-dom';
+import { useEffect } from 'react';
 import styles from './Modal.module.css';
-import { Component } from 'react';
-
-import {createPortal} from 'react-dom';
-import PropTypes from 'prop-types';
-
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
+export const Modal = ({ children,onClose }) => {
 
-  static propTypes = {
-    onClose: PropTypes.func.isRequired,
-    children: PropTypes.any.isRequired,
-  }
+  useEffect(() => {
+    const handleKeyClose = event => {
+      if (event.code === 'Escape') {
+        onClose();
+      }
+    };
 
+    window.addEventListener('keydown', handleKeyClose);
 
-  componentDidMount() {
-    window.addEventListener('keydown',this.handleKeyClose );
-  }
+    return () => {
+      window.removeEventListener('keydown', handleKeyClose);
+    };
+  }, [onClose]);
 
-  componentWillUnmount() {
-    window.removeEventListener('keydown',this.handleKeyClose );
-  }
-
-  handleKeyClose  = event => {
-    if (event.code === 'Escape') {
-      this.props.onClose();
+  const handleCloseBackdrop = event => {
+    if (event.currentTarget === event.target) {
+      onClose();
     }
   };
 
-  handleCloseBackdrop  = event => {
-     if (event.currentTarget === event.target) {
-       this.props.onClose();
-     }
-   };
-
-  render() {
-    const { children } =this.props;
-    return createPortal(
-      <div className={styles.overlay} onClick={this.handleCloseBackdrop}>
+  return createPortal(
+      <div className={styles.overlay} onClick={handleCloseBackdrop}>
         <div className={styles.modal}>{children}</div>
       </div>,
       modalRoot
     );
 
   }
-}
+
 
