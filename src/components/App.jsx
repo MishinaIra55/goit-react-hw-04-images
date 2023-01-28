1d243a47696dc32bb65ac5ecb538d73c4057c35a
MishynaIra <gortenzia1987@gmail.com>
2023-01-28 10:12:54 +0200

    отформатировала код

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -61,16 +61,78 @@
 //
 
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Searchbar } from './Searchbar/Searchbar';
 import { GalleryForm } from './GalleryForm';
 import { ToastContainer } from 'react-toastify';
 import { Modal } from './Modal/Modal';
+import { fetchAxiosGallery } from 'services/pixibay-api';
+
 
 export const App = () => {
   const [search, setSearch] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [largeImage, setLargeImage] = useState('');
+
+  const [images, setImages] = useState([]);
+  const [error, setError] = useState('');
+  const [status, setStatus] = useState('idle');
+  const [page, setPage] = useState(1);
+
+  useEffect(()=> {
+    const fetchData = async ({search, page}) => {
+      setStatus('pending');
+
+      try {
+        const response = await fetchAxiosGallery ({search, page}) ;
+        handleResolve(response);
+      }catch (error) {
+      setStatus('rejected');
+      console.log(error);
+      }
+    };
+
+      const handleResolve = ({ hits, total, totalHits }) => {
+        const sortedImages = hits.map(
+          ({ id, webformatURL, tags, largeImageURL }) => ({
+            id,
+            webformatURL,
+            tags,
+            largeImageURL,
+          })
+        );
+
+        if (!total) {
+          alert(
+            'Sorry, there are no images matching your search, please try another key word'
+          );
+          setStatus('idle');
+          return;
+        }
+
+        if (totalHits < page * 12) {
+          setImages(prevState => [...prevState, ...sortedImages]);
+          setStatus('idle');
+          alert(
+            "We're sorry, but you've reached the end of search results"
+          );
+          return;
+        }
+
+        setImages(prevState => [...prevState, ...sortedImages]);
+        setStatus('success');
+
+        if (page === 1) {
+          alert(`That's what we found`);
+        }
+      };
+
+    if (!search) {
+      return;
+    }
+
+    fetchData({ page, search });
+  }, [page, search]);
 
 
   const handleSearchbar = search => {
